545bfaa9dbbe1dfddc3699ef24db7889b41c1b4e
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2024-11-27 18:33:00 -0500

    UPDATE

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,43 @@ import { MdEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
 export default function MyApp() {
   const [todo, setTodo] = useState("");
-  const [main, setMain] = useState<string[]>([]);
-  const [completed, setCompleted] = useState<number>(0);
-  const [remaining, setRemaining] = useState<number>(0);
+  const [complete, setComplete] = useState(false);
+  const [main, setMain] = useState<
+    { todo: string; id: number; remaining: number; complete: boolean }[]
+  >(
+    localStorage.getItem("next-todo")
+      ? JSON.parse(localStorage.getItem("next-todo") || "[]")
+      : []
+  );
+
+  const [remaining, setRemaining] = useState<number>(
+    main.length > 0 ? main[main.length - 1].remaining : 0
+  );
+  const [editId, setEditId] = useState<number | null>();
+
   const formHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setRemaining(remaining + 1);
-    console.log(remaining);
-    setMain([...main, todo]);
-    console.log(main);
+
+    if (!todo) {
+      return false;
+    } else if (todo && editId) {
+      console.log(editId);
+      setMain(
+        main.map((item) => {
+          if (item.id === editId) {
+            return { ...item, todo: todo };
+          }
+          setEditId(null);
+          return item;
+        })
+      );
+    } else {
+      setRemaining(remaining + 1);
+      setMain([
+        ...main,
+        { todo, id: Date.now(), remaining: remaining + 1, complete },
+      ]);
+    }
     setTodo("");
   };
   const initialTodo = (
@@ -22,43 +50,69 @@ export default function MyApp() {
     </h2>
   );
   useEffect(() => {
-    localStorage.setItem("todo-list", JSON.stringify(main));
+    localStorage.setItem("next-todo", JSON.stringify(main));
   }, [main, remaining]);
-
+  const completeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    id: number
+  ) => {
+    if ((event.target as HTMLInputElement).checked) {
+      setRemaining(remaining - 1);
+      setComplete(true);
+      setMain((perv) =>
+        perv.map((item) => {
+          if (item.id === id) {
+            return { ...item, complete: true, remaining: remaining - 1 };
+          }
+          return item;
+        })
+      );
+    } else {
+      setRemaining(remaining + 1);
+      setComplete(false);
+      setMain((perv) =>
+        perv.map((item) => {
+          if (item.id === id) {
+            return { ...item, complete: false, remaining: remaining + 1 };
+          }
+          return item;
+        })
+      );
+    }
+  };
   const renderTodo = main.map((item, idx) => {
     return (
-      <li key={idx} className="flex items-center justify-between gap-3">
+      <li
+        key={item.id}
+        id={item.id.toString()}
+        className="flex items-center justify-between gap-3"
+      >
         <div className="checkbox">
           <label className="custom-checkbox">
             <input
               type="checkbox"
               id="checkbox"
               name="checkbox"
-              onClick={(event) => {
-                if ((event.target as HTMLInputElement)?.checked) {
-                  setCompleted(completed + 1);
-                  setRemaining(remaining - 1);
-                } else {
-                  setCompleted(completed - 1);
-                  setRemaining(remaining + 1);
-                }
+              checked={main.length > 0 ? main[idx].complete : false}
+              onChange={(event) => {
+                completeHandler(event, item.id);
               }}
             />
             <span className="checkmark"></span>
           </label>
         </div>
-        <p className="text-sm text-slate-100">{item}</p>
+        <p className="text-sm text-slate-100">{item.todo}</p>
         <div className="icons flex gap-2">
           <div className="edit">
             <MdEdit
-              onClick={() => editHandler(idx, item)}
+              onClick={() => editHandler(item.id)}
               size={22}
               className="text-emerald-700 hover:text-emerald-800 cursor-pointer"
             />
           </div>
           <div className="delete">
             <MdDelete
-              onClick={() => delHandler(idx)}
+              onClick={() => delHandler(item.id)}
               size={22}
               className="text-red-600 hover:text-red-700 cursor-pointer"
             />
@@ -69,14 +123,19 @@ export default function MyApp() {
   });
   const delHandler = (id: number) => {
     const shallowTodo = [...main];
-    shallowTodo.splice(id, 1);
-    setRemaining(remaining - 1);
+    const index = shallowTodo.findIndex((todo) => todo.id === id);
+    if (!(shallowTodo.length > 0 && shallowTodo[index].complete)) {
+      setRemaining(remaining - 1);
+      main[main.length - 1].remaining = remaining - 1;
+    }
+
+    shallowTodo.splice(index, 1);
     setMain(shallowTodo);
   };
-  const editHandler = (id: number, text: string) => {
-    let shallowTodo = [...main];
-    setTodo(text);
-    setMain(shallowTodo);
+  const editHandler = (id: number) => {
+    const findTodo = main.find((item) => item.id === id);
+    setTodo(findTodo?.todo || "");
+    setEditId(findTodo?.id);
   };
   return (
     <div className={`todo-list-container `}>
@@ -112,12 +171,18 @@ export default function MyApp() {
         </ul>
       </div>
       <div className="todo-list-footer">
-        <p className="text-sm">
-          Completed: <span className="completed-count">{completed}</span>
-        </p>
         <p>
           Remaining: <span className="remaining-count">{remaining}</span>
         </p>
+        <button
+          className="add-todo-btn uppercase hover:bg-red-500"
+          onClick={() => {
+            setMain([]);
+            window.location.reload();
+          }}
+        >
+          Clear All
+        </button>
       </div>
     </div>
   );
